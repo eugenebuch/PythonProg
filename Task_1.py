0c824845d3519a2848d818def6b6d1f77722b34e
Eugene Buchenkov <bieyffull@gmail.com>
2020-09-02 23:19:48 +0300

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,32 @@
+def sort(a):
+# Процедура, выполняющая сортировку вставками, работающая по ссылке на список
+    for i in range(1, len(a)):
+        ins = a[i]
+        j = i - 1
+        while j >= 0 and a[j] > ins:
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = ins
+        
+def printList(a):
+# Процедура, выполняющая поэлементную печать элементов списка
+    for elem in a:
+        print(round(elem, 2))
+
+def fileSave(a):
+# Процедура, сохраняющая элементы списка в файл (построчно). Если файл не существует, то он создается в той же папке, что и программа
+    with open("output.txt", "w") as file:
+        for elem in a:
+            file.write(str(round(elem, 2)) + '\n')
+
+def main():
+# Основная процедура, сюда вынесены управляющие элементы и вызовы других процедур
+    n = 10
+    lst = []
+    for i in range(n):
+        lst.append(float(input())*0.13)
+    sort(lst)
+    printList(lst)
+    fileSave(lst)
+    
+main()
